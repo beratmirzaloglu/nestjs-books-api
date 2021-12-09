4b62af24b3e2785ea15648fc4fe44127d81de65e
Berat Mirzaloğlu <berat.mirzaloglu@turkticaret.net>
2021-12-09 20:23:48 +0300

    db connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    BooksModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+  ],
   controllers: [],
   providers: [],
 })
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookSchema } from './book.model';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Book', schema: BookSchema }])],
   controllers: [BooksController],
   providers: [BooksService],
 })
