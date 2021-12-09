0230de862ae71e367c5a39db420e81bb454e1238
Berat Mirzaloğlu <berat.mirzaloglu@turkticaret.net>
2021-12-09 20:23:59 +0300

    mongoose modeling

--- src/books/book.model.ts
+++ src/books/book.model.ts
@@ -1,3 +1,12 @@
+import * as mongoose from 'mongoose';
+
+export const BookSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  author: { type: String, required: true },
+  category: { type: String, required: true },
+  numberOfPages: { type: Number, required: true },
+});
+
 export class Book {
   constructor(
     public id: string,
